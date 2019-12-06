ce4260567a577faec5019ea71f27c586aa666409
Arijit Biswas <dibbyo456@gmail.com>
2019-12-06 12:33:55 +0530

    Fix implode() PHP 7.4
    implode(): Passing glue string after array is deprecated.

--- MysqliDb.php
+++ MysqliDb.php
@@ -1835,7 +1835,7 @@ class MysqliDb
         $dataColumns = array_keys($tableData);
         if ($isInsert) {
             if (isset ($dataColumns[0]))
-                $this->_query .= ' (`' . implode($dataColumns, '`, `') . '`) ';
+                $this->_query .= ' (`' . implode('`, `', $dataColumns) . '`) ';
             $this->_query .= ' VALUES (';
         } else {
             $this->_query .= " SET ";
